f75c6f9b00a1f457389dfd49073cb5d90398bac6
Alan Li <alan.li@me.com>
2017-05-08 17:18:54 -0400

    NodeAtPositionFinder to avoid generating temp objects.

--- src/NodeVisitor/NodeAtPositionFinder.php
+++ src/NodeVisitor/NodeAtPositionFinder.php
@@ -25,18 +25,44 @@ class NodeAtPositionFinder extends NodeVisitorAbstract
     private $position;
 
     /**
+     * @var Position
+     */
+    private $startPos;
+
+    /**
+     * @var Position
+     */
+    private $endPos;
+
+    /**
      * @param Position $position The position where the node is located
      */
     public function __construct(Position $position)
     {
         $this->position = $position;
+
+        $this->startPos = new Position(0, 0);
+        $this->endPos  = new Position(0, 0);
     }
 
     public function leaveNode(Node $node)
     {
         if ($this->node === null) {
-            $range = Range::fromNode($node);
-            if ($range->includes($this->position)) {
+            //$range = Range::fromNode($node);
+
+            // avoid creating objects.
+            $startLine = $node->getAttribute('startLine') - 1;
+            $endLine = $node->getAttribute('endLine') - 1;
+
+            $this->startPos->character = $node->getAttribute('startColumn') - 1;
+            $this->endPos->character = $node->getAttribute('endColumn');
+
+            //print_r($range);
+            //if ($this->startPos->compare($this->position) <= 0 && $this->endPos->compare($this->position) >= 0/*$range->includes($this->position)*/) {
+            if ($startLine <= $this->position->line && $endLine >= $this->position->line) {
+              $startChar  = $node->getAttribute('startColumn') - 1;
+              $endChar  = $node->getAttribute('endColumn');
+              
                 $this->node = $node;
                 return NodeTraverser::STOP_TRAVERSAL;
             }
